c5953840e91e98ad3af1a639377853d18a687f95
kc15155 <33430603+kc15155@users.noreply.github.com>
2017-12-10 17:57:03 +0200

    Add files via upload

--- a2/src/main/java/bgu/spl/a2/Action.java
+++ a2/src/main/java/bgu/spl/a2/Action.java
@@ -1,81 +1,90 @@
 package bgu.spl.a2;
 
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 
 public abstract class Action<R> {
-
-
+	
+	private String name;
+	private Promise<R> result;
+	private ActorThreadPool myPool;
+	private callback myCallback;
+	private String actorId;
+	private PrivateState actorState;
+	
     protected abstract void start();
     
 
-    /**
-    *
-    * start/continue handling the action
-    *
-    * this method should be called in order to start this action
-    * or continue its execution in the case where it has been already started.
-    *
-    * IMPORTANT: this method is package protected, i.e., only classes inside
-    * the same package can access it - you should *not* change it to
-    * public/private/protected
-    *
-    */
    /*package*/ final void handle(ActorThreadPool pool, String actorId, PrivateState actorState) {
+	   
+	   this.actorState=actorState;
+	   this.actorId=actorId;
+	   this.myPool=pool;
+	   if (myCallback==null)
+		   start();
+	   else
+	   {
+		   myCallback.call();
+		   complete(result.get());
+	   }
    }
     
     
-    /**
-     * add a callback to be executed once *all* the given actions results are
-     * resolved
-     * 
-     * Implementors note: make sure that the callback is running only once when
-     * all the given actions completed.
-     *
-     * @param actions
-     * @param callback the callback to execute once all the results are resolved
-     */
-    protected final void then(Collection<? extends Action<?>> actions, callback callback) {
-       	//TODO: replace method body with real implementation
-        throw new UnsupportedOperationException("Not Implemented Yet.");
-   
+   	protected final void then(Collection<? extends Action<?>> actions, callback callback) {
+   		
+   		final callback toAdd=callback;
+    	final int actionSize=actions.size();
+   		final int [] counter = new int [1];
+   		counter[0]=0;
+   		callback newCall = new callback() {
+			
+			public void call() {
+				counter[0]++;
+				if (counter[0]==actionSize)
+				{
+					myPool.submit(getAction(), actorId, actorState);
+					myCallback=toAdd;
+				}
+			}
+		};
+    	for (Action<?> temp : actions)
+    	{
+    		temp.getResult().subscribe(newCall);
+    	}	
     }
 
-    /**
-     * resolve the internal result - should be called by the action derivative
-     * once it is done.
-     *
-     * @param result - the action calculated result
-     */
+ 
     protected final void complete(R result) {
-       	//TODO: replace method body with real implementation
-        throw new UnsupportedOperationException("Not Implemented Yet.");
+       	this.result.resolve(result);
    
     }
     
-    /**
-     * @return action's promise (result)
-     */
+
     public final Promise<R> getResult() {
-    	//TODO: replace method body with real implementation
-        throw new UnsupportedOperationException("Not Implemented Yet.");
+    	return result;
     }
     
-    /**
-     * send an action to an other actor
-     * 
-     * @param action
-     * 				the action
-     * @param actorId
-     * 				actor's id
-     * @param actorState
-	 * 				actor's private state (actor's information)
-	 *    
-     * @return promise that will hold the result of the sent action
-     */
-	public Promise<?> sendMessage(Action<?> action, String actorId, PrivateState actorState){
-        //TODO: replace method body with real implementation
-        throw new UnsupportedOperationException("Not Implemented Yet.");
+
+	public Promise<?> sendMessage(Action<?> action, String actorId, PrivateState actorState)
+	{
+        myPool.submit(action, actorId, actorState);
+        return action.getResult();
+	}
+	
+
+	public void setActionName(String actionName){
+        this.name=actionName;
 	}
 
+	
+	public String getActionName(){
+        return name;
+	}
+	
+	private Action<R> getAction()
+	{
+		return this;
+	}
 }
--- a2/src/main/java/bgu/spl/a2/ActorThreadPool.java
+++ a2/src/main/java/bgu/spl/a2/ActorThreadPool.java
@@ -1,23 +1,50 @@
 package bgu.spl.a2;
 
+
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import bgu.spl.a2.sim.privateStates.CoursePrivateState;
+import bgu.spl.a2.sim.privateStates.DepartmentPrivateState;
+import bgu.spl.a2.sim.privateStates.StudentPrivateState;
 
 
 
 public class ActorThreadPool {
 	
-	ConcurrentHashMap<PrivateState, String> stateMap;
-	ConcurrentHashMap<String, Action> actionMap;
+	ConcurrentHashMap<String, String> stateMap;
+	ConcurrentHashMap<String, Queue<Action>> actionMap;
+	ConcurrentHashMap<String, AtomicBoolean> blockMap;
 	Thread [] threadArray;
-
+	VersionMonitor myVer;
 	
 	public ActorThreadPool(int nthreads) {
-		stateMap = new ConcurrentHashMap<PrivateState, String>();
-		actionMap = new ConcurrentHashMap<String, Action>();
+		stateMap = new ConcurrentHashMap<String, String>();
+		actionMap = new ConcurrentHashMap<String, Queue<Action>>();
+		blockMap = new ConcurrentHashMap<String, AtomicBoolean>();
 		threadArray = new Thread [nthreads];
+		myVer= new VersionMonitor();
 		Runnable r = new Runnable() {
 			public void run() {
-			
+				for (String temp : actionMap.keySet())
+				{
+					if (!actionMap.get(temp).isEmpty())
+					{
+						if (blockMap.get(temp).compareAndSet(true,false))
+						{
+							actionMap.get(temp).poll().handle(getPool(), temp, checkPrivateState(temp));
+							myVer.inc();
+						}
+							blockMap.get(temp).set(true);	
+					}
+				}
+				try {myVer.await(myVer.getVersion());}
+				catch (InterruptedException e) {
+					run();
+				}
+				
 			}
 			
 		};
@@ -29,8 +56,21 @@ public class ActorThreadPool {
 
 
 	public void submit(Action<?> action, String actorId, PrivateState actorState) {
-		stateMap.putIfAbsent(actorState, actorId);
-		actionMap.putIfAbsent(actorId, action);
+		if (actionMap.containsKey(actorId))
+			actionMap.get(actorId).add(action);
+		else
+		{
+			actionMap.put(actorId, new LinkedList<Action>());
+			actionMap.get(actorId).add(action);
+		}
+		blockMap.putIfAbsent(actorId, new AtomicBoolean(true));
+		if (actorState instanceof CoursePrivateState)
+			stateMap.putIfAbsent("course", actorId);
+		if (actorState instanceof StudentPrivateState)
+			stateMap.putIfAbsent("student", actorId);
+		if (actorState instanceof DepartmentPrivateState)
+			stateMap.putIfAbsent("department", actorId);
+		myVer.inc();
 	}
 
 
@@ -50,5 +90,23 @@ public class ActorThreadPool {
 			temp.start();
 		}
 	}
+	
+	private PrivateState checkPrivateState (String actorId)
+	{
+		if (stateMap.get("student").contains(actorId))
+			return new StudentPrivateState();
+		if (stateMap.get("course").contains(actorId))
+			return new CoursePrivateState();
+		if (stateMap.get("department").contains(actorId))
+			return new DepartmentPrivateState();
+		else
+			return null;
+	}
+	
+	private ActorThreadPool getPool ()
+	{
+		return this;
+	}
+	
 
 }
